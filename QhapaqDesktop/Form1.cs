d3a04f0c083d58940668eb358513d121b3176fc1
sushi99 <atsushirafuji@outlook.com>
2019-02-13 12:52:20 +0900

    Add MetroFramework and apply

--- QhapaqDesktop/Form1.cs
+++ QhapaqDesktop/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
 
 namespace QhapaqDesktop {
-  public partial class Form1 : Form {
+  public partial class Form1 : MetroForm {
     public Form1() {
       InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e) {
+
     }
   }
 }
